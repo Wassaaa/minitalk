0c0ab46c2efb52203592d22c1aa95697f754e566
aklein <aklein@student.hive.fi>
2024-04-19 02:18:19 +0300

    lets try wihtout blocking

--- c_main.c
+++ c_main.c
@@ -6,69 +6,63 @@
 /*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 14:33:25 by aklein            #+#    #+#             */
-/*   Updated: 2024/04/18 21:52:46 by aklein           ###   ########.fr       */
+/*   Updated: 2024/04/19 02:16:57 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
 
+volatile sig_atomic_t	g_signal_recieved = 0;
+
 void	send_char(char c, int pid)
 {
 	int	i;
+	int	timeout;
 
+	timeout = 0;
 	i = 8;
+	//usleep(100000);
 	while (i--)
 	{
+		g_signal_recieved = 0;
 		if (c >> i & 1)
+		{
 			kill(pid, SIGUSR1);
+			//ft_printf("1");
+		}
 		else
+		{
 			kill(pid, SIGUSR2);
-		pause();
+			//ft_printf("0");
+		}
+		while (!g_signal_recieved)
+		{
+			usleep(1000);
+			timeout += 1000;
+			if (timeout > 1000000)
+			{
+				ft_printf("\nserver not responding, try again!\n");
+				exit(1);
+			}
+		}
 	}
-}
-
-void	print_bits(char c)
-{
-	int	i;
-
-	i = 8;
-	while (i--)
-	{
-		ft_printf("%d", c >> i & 1);
-	}
-	ft_printf(" = %d\n", c);
+	ft_printf("\n");
 }
 
 void	send_str(char *str, int pid)
 {
 	while (*str)
 	{
-		print_bits(*str);
+		//ft_printf("sending '%c' - ", *str);
 		send_char(*str++, pid);
 	}
-	send_char('\n', pid);
 }
 
 void	acknowledge(int sig)
 {
-	(void)sig;
+	if (sig == SIGUSR1 || sig == SIGUSR2)
+		g_signal_recieved = 1;
 }
-
-// void	acknowledge(int sig)
-// {
-// 	static char	c = 0;
-// 	static int	i = 0;
-
-// 	if (sig == SIGUSR1)
-// 		c = c | (1 << (7 - i));
-// 	i++;
-// 	if (i > 7)
-// 	{
-// 		ft_printf("%c", c);
-// 		i = 0;
-// 		c = 0;
-// 	}
-// }
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -92,9 +86,6 @@ int	main(int argc, char **argv)
 
 	if (argc == 3)
 	{
-		int	my_pid;
-
-		my_pid = (int)getpid();
 		ft_bzero(&action, sizeof(action));
 		action.sa_handler = acknowledge;
 		sigaction(SIGUSR1, &action, NULL);
--- s_main.c
+++ s_main.c
@@ -6,30 +6,22 @@
 /*   By: aklein <aklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 14:33:17 by aklein            #+#    #+#             */
-/*   Updated: 2024/04/18 21:53:44 by aklein           ###   ########.fr       */
+/*   Updated: 2024/04/19 02:14:49 by aklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minitalk.h>
+
+volatile sig_atomic_t	is_busy = 0;
 
 static int	current_pid(int reset, int new_pid)
 {
 	static	int expected_pid = 0;
 	if (reset)
 		expected_pid = 0;
-	if (new_pid != 0)
+	if (new_pid)
 		expected_pid = new_pid;
 	return (expected_pid);
-}
-
-static int	signal_flag(int reset, int updated)
-{
-	static int	last_sig = 1;
-	if (updated)
-		last_sig++;
-	if (reset)
-		last_sig = 1;
-	return (last_sig);
 }
 
 void	handle_sigusr(int sig, siginfo_t *siginfo, void *context)
@@ -37,6 +29,7 @@ void	handle_sigusr(int sig, siginfo_t *siginfo, void *context)
 	static char	c = 0;
 	static int	i = 0;
 	int			current_expected_pid;
+
 	(void)context;
 	if (current_pid(0, 0) == 0)
 	{
@@ -47,8 +40,7 @@ void	handle_sigusr(int sig, siginfo_t *siginfo, void *context)
 	current_expected_pid = current_pid(0, 0);
 	if (current_expected_pid != siginfo->si_pid)
 		return ;
-	// ft_printf("signal '%d' from PID: %d\n", sig, siginfo->si_pid);
-	signal_flag(0, 1);
+	is_busy++;
 	if (sig == SIGUSR1)
 		c = c | (1 << (7 - i));
 	i++;
@@ -79,7 +71,7 @@ int	main()
 	struct sigaction	action;
 	sigset_t			set;
 	int					pid;
-	int					flag;
+	int					last_busy;
 
 	pid = (int)getpid();
 	sigemptyset(&set);
@@ -95,17 +87,17 @@ int	main()
 	/*remove me*/
 	save_pid(pid);
 	/*---------*/
-	flag = 1;
+	last_busy = 0;
 	while (42)
 	{
-		usleep(100000);
-		// sleep(2);
-		if (signal_flag(0, 0) == flag)
+		usleep(10000);
+		if (is_busy == last_busy && last_busy != 0)
 		{
-			signal_flag(1, 0);
+			is_busy = 0;
 			current_pid(1, 0);
+			ft_printf("\n");
 		}
-		flag = signal_flag(0, 0);
+		last_busy = is_busy;
 	}
 	return(0);
 }
